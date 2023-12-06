9d7c80cfa3d500452f50f8a4b64759dec76d5589
Marcus Silva <marcusluis.mail@gmail.com>
2023-12-06 16:55:30 -0300

    usando o método String.format para criar strings formatas e armazena-las em uma variavel

---
+++ string-format/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Marcus";
+        int age = 100;
+        boolean sleeping = true;
+
+        String result = String.format("Nome: %s, age: %d, sleeping: %b", name, age, sleeping);
+
+        System.out.println(result);
+    }
+}
